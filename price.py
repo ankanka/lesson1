6fa024efde3c6e6aab88f3031b1309381ffa1538
Anna <anechka_fwd@mail.ru>
2021-02-13 17:56:51 +0300

    first commit

---
+++ price.py
@@ -0,0 +1,5 @@
+
+def get_summ(one, two, delimiter='&'):
+    print(f'{one}{delimiter}{two}'.upper())
+
+get_summ('Learn', 'python')
---
+++ types.py
@@ -0,0 +1,4 @@
+dict1 = { "city": "Москва", "temperature": "20" }
+dict1["temperature"] = int(dict1["temperature"]) - 5
+dict1["date"] = "27.05.2019"
+print(dict1)
